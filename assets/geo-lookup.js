ac2fa6cb7a50b896cb942058cfda666c5b299816
Rory Burns <rdwburns@gmail.com>
2021-07-16 17:17:23 +0100

    Geolocation

--- assets/geo-lookup.js
+++ assets/geo-lookup.js
@@ -1,43 +1,85 @@
 let apiKey = 'c5fca7ad2280d64fd6493f6daafef804424cfb11fa8124350533f64e';
 let path = window.location.pathname;
 let cookieValue = (document.cookie.match(/^(?:.*;)?\s*vr_geo\s*=\s*([^;]+)(?:.*)?$/) || [, null])[1]
-
-
+let currentSite = 'United Kingdom'
+const sites = {
+    uk:{
+        name: 'United Kingdom',
+        locale: 'en_GB',
+        menu_name: 'gb - GBP',
+        url:'https://vicereversa.co.uk'
+    },
+    germany:{
+        name: 'Germany',
+        locale: 'de_DE',
+        menu_name: 'de - EUR',
+        url:'https://vicereversa.de'
+    },
+    france:{
+        name: 'France',
+        locale: 'fr_FR',
+        menu_name: 'fr - EUR',
+        url:'https://vicereversa.fr'
+    },
+    ireland:{
+        name: 'Ireland',
+        locale: 'en_IE',
+        menu_name: 'ie - EUR',
+        url:'https://vicereversa.ie'
+    },
+    australia:{
+        name: 'Australia',
+        locale: 'en_AU',
+        menu_name: 'au - AUD',
+        url:'https://vicereversa.com.au'
+    }
+}
 
 function json(url) {
     return fetch(url).then(res => res.json());
 }
 
+function switchCountries(home, destination, destinationUrl){
+    document.body.style.overflow = 'hidden'
+    document.getElementById('modal-text').innerHTML = `<p>You are currently on our ${home} site, but you appear to be in ${destination}, would you like to visit that site instead?</p><div class="modal-buttons"><button href="${destinationUrl}">Shop in ${destination}</button><button data-dismiss="modal">Stay Here</button></div>`
+    document.getElementById('geo-modal').classList.add('open');
+    document.cookie = `vr_geo=true; Secure`;
+}
 
-if (!!cookieValue && cookieValue != window.location.hostname) {
-    console.log('cookie found')
-    window.location.href = "https://" + cookieValue + path
-} else {
+if (!cookieValue){
     json(`https://api.ipdata.co?api-key=${apiKey}`).then(data => {
-        if (data.country_code == 'UK' && window.location.hostname != "vicereversa.co.uk") {
-            window.location.href = "https://vicereversa.co.uk" + path;
-            document.cookie = `vr_geo=${window.location.hostname}}; Secure`;
+        console.log(data)
+        if (data.country_code == 'GB' && window.location.hostname != "vicereversa.co.uk") {
+            switchCountries(currentSite, data.country_name, sites.uk.url)
         } else if (data.country_code == 'DE' && window.location.hostname != "vicereversa.de") {
-            window.location.href = "https://vicereversa.de" + path;
-            document.cookie = `vr_geo=${window.location.hostname}}; Secure`;
+            switchCountries(currentSite, data.country_name, sites.germany.url)
         } else if (data.country_code == 'FR' && window.location.hostname != "vicereversa.fr") {
-            window.location.href = "https://vicereversa.fr" + path;
-            document.cookie = `vr_geo=${window.location.hostname}}; Secure`;
+            switchCountries(currentSite, data.country_name, sites.france.url)
         } else if (data.country_code == 'IE' && window.location.hostname != "vicereversa.ie") {
-            window.location.href = "https://vicereversa.ie" + path;
-            document.cookie = `vr_geo=${window.location.hostname}}; Secure`;
+            switchCountries(currentSite, data.country_name, sites.ireland.url)
         } else if (data.country_code == 'AU') {
-            window.location.href = "https://vicereversaskin.com.au" + path;
-            document.cookie = `vr_geo=${window.location.hostname}}; Secure`;
+            switchCountries(currentSite, data.country_name, sites.australia.url)
         } else if (data.continent_code == 'EU' && window.location.hostname != "vicereversaskin.eu" && data.languages.map(name => name).includes('English')) {
-            window.location.href = "https://vicereversaskin.eu" + path;
-            document.cookie = `vr_geo=${window.location.hostname}}; Secure`;
+            switchCountries(currentSite, data.country_name, sites.ireland.url)
         } else if (data.continent_code == 'EU' && window.location.hostname != "vicereversaskin.eu/de" && data.languages.map(name => name).includes('German')) {
-            window.location.href = "https://vicereversaskin.eu/de" + path;
-            document.cookie = `vr_geo=${window.location.hostname}}; Secure`;
+            switchCountries(currentSite, data.country_name, sites.germany.url)
         } else if (data.continent_code == 'EU' && window.location.hostname != "vicereversaskin.eu/fr" && data.languages.map(name => name).includes('French')) {
-            window.location.href = "https://vicereversaskin.eu/fr" + path;
-            document.cookie = `vr_geo=${window.location.hostname}}; Secure`;
+            switchCountries(currentSite, data.country_name, sites.france.url)
         }
     });
 }
+
+
+document.addEventListener('click', function (e) {
+    e = e || window.event;
+    var target = e.target;
+
+    e.preventDefault();
+
+    // Close modal window with 'data-dismiss' attribute or when the backdrop is clicked
+    if ((target.hasAttribute('data-dismiss') && target.getAttribute('data-dismiss') == 'modal') || target.classList.contains('modal')) {
+        var modal = document.querySelector('[class="modal open"]');
+        document.body.style.overflow = ''
+        modal.classList.remove('open');
+    }
+}, false);
