a1c54284c294a411a154a462feb60529717ee3c2
Rory Burns <rdwburns@gmail.com>
2021-07-17 20:10:06 +0100

    Update geo-lookup.js

--- assets/geo-lookup.js
+++ assets/geo-lookup.js
@@ -2,38 +2,6 @@ let apiKey = 'c5fca7ad2280d64fd6493f6daafef804424cfb11fa8124350533f64e';
 let path = window.location.pathname;
 let cookieValue = (document.cookie.match(/^(?:.*;)?\s*vr_geo\s*=\s*([^;]+)(?:.*)?$/) || [, null])[1]
 let currentSite = 'United Kingdom'
-const sites = {
-    uk:{
-        name: 'United Kingdom',
-        locale: 'en_GB',
-        menu_name: 'gb - GBP',
-        url:'https://vicereversa.co.uk'
-    },
-    germany:{
-        name: 'Germany',
-        locale: 'de_DE',
-        menu_name: 'de - EUR',
-        url:'https://vicereversa.de'
-    },
-    france:{
-        name: 'France',
-        locale: 'fr_FR',
-        menu_name: 'fr - EUR',
-        url:'https://vicereversa.fr'
-    },
-    ireland:{
-        name: 'Ireland',
-        locale: 'en_IE',
-        menu_name: 'ie - EUR',
-        url:'https://vicereversa.ie'
-    },
-    australia:{
-        name: 'Australia',
-        locale: 'en_AU',
-        menu_name: 'au - AUD',
-        url:'https://vicereversa.com.au'
-    }
-}
 
 function json(url) {
     return fetch(url).then(res => res.json());
@@ -60,23 +28,18 @@ if (!cookieValue){
         
     }, false);    
     json(`https://api.ipdata.co?api-key=${apiKey}`).then(data => {
-        console.log(data)
         if (data.country_code == 'GB' && window.location.hostname != "vicereversa.co.uk") {
-            switchCountries(currentSite, data.country_name, sites.uk.url)
+            switchCountries(currentSite, data.country_name, 'https://vicereversa.co.uk')
         } else if (data.country_code == 'DE' && window.location.hostname != "vicereversa.de") {
-            switchCountries(currentSite, data.country_name, sites.germany.url)
+            switchCountries(currentSite, data.country_name, 'https://vicereversa.de')
         } else if (data.country_code == 'FR' && window.location.hostname != "vicereversa.fr") {
-            switchCountries(currentSite, data.country_name, sites.france.url)
+            switchCountries(currentSite, data.country_name, 'https://vicereversa.fr')
         } else if (data.country_code == 'IE' && window.location.hostname != "vicereversa.ie") {
-            switchCountries(currentSite, data.country_name, sites.ireland.url)
+            switchCountries(currentSite, data.country_name, 'https://vicereversa.ie')
         } else if (data.country_code == 'AU') {
-            switchCountries(currentSite, data.country_name, sites.australia.url)
-        } else if (data.continent_code == 'EU' && window.location.hostname != "vicereversaskin.eu" && data.languages.map(name => name).includes('English')) {
-            switchCountries(currentSite, data.country_name, sites.ireland.url)
-        } else if (data.continent_code == 'EU' && window.location.hostname != "vicereversaskin.eu/de" && data.languages.map(name => name).includes('German')) {
-            switchCountries(currentSite, data.country_name, sites.germany.url)
-        } else if (data.continent_code == 'EU' && window.location.hostname != "vicereversaskin.eu/fr" && data.languages.map(name => name).includes('French')) {
-            switchCountries(currentSite, data.country_name, sites.france.url)
+            switchCountries(currentSite, data.country_name, 'https://vicereversa.com.au')
+        } else if (data.continent_code == 'EU' && window.location.hostname != "vicereversaskin.eu" ) {
+            switchCountries(currentSite, data.country_name, 'https://vicereversa.ie')
         }
     });
 }
