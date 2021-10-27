6e2129538da28299d4ec7bbb4e288cdf7f7e4a20
Rory Burns <rdwburns@gmail.com>
2021-10-27 21:07:56 +0100

    Dawn Updates

--- assets/product-model.js
+++ assets/product-model.js
@@ -1,55 +1,64 @@
-class ProductModel extends DeferredMedia {
-  constructor() {
-    super();
-  }
+if (!customElements.get('product-form')) {
+  customElements.define('product-form', class ProductForm extends HTMLElement {
+    constructor() {
+      super();
 
-  loadContent() {
-    super.loadContent();
-
-    Shopify.loadFeatures([
-      {
-        name: 'model-viewer-ui',
-        version: '1.0',
-        onLoad: this.setupModelViewerUI.bind(this),
-      },
-    ]);
-  }
-
-  setupModelViewerUI(errors) {
-    if (errors) return;
-
-    this.modelViewerUI = new Shopify.ModelViewerUI(this.querySelector('model-viewer'));
-  }
-}
-customElements.define('product-model', ProductModel);
-
-window.ProductModel = {
-  loadShopifyXR() {
-    Shopify.loadFeatures([
-      {
-        name: 'shopify-xr',
-        version: '1.0',
-        onLoad: this.setupShopifyXR.bind(this),
-      },
-    ]);
-  },
-
-  setupShopifyXR(errors) {
-    if (errors) return;
-
-    if (!window.ShopifyXR) {
-      document.addEventListener('shopify_xr_initialized', () =>
-        this.setupShopifyXR()
-      );
-      return;
+      this.form = this.querySelector('form');
+      this.form.querySelector('[name=id]').disabled = false;
+      this.form.addEventListener('submit', this.onSubmitHandler.bind(this));
+      this.cartNotification = document.querySelector('cart-notification');
     }
 
-    document.querySelectorAll('[id^="ProductJSON-"]').forEach((modelJSON) => {
-      window.ShopifyXR.addModels(JSON.parse(modelJSON.textContent));
-      modelJSON.remove();
-    });
-    window.ShopifyXR.setupXRElements();
-  },
-};
+    onSubmitHandler(evt) {
+      evt.preventDefault();
+      const submitButton = this.querySelector('[type="submit"]');
+      if (submitButton.classList.contains('loading')) return;
 
-window.addEventListener('DOMContentLoaded', () => { window.ProductModel?.loadShopifyXR(); });
+      this.handleErrorMessage();
+      this.cartNotification.setActiveElement(document.activeElement);
+
+      submitButton.setAttribute('aria-disabled', true);
+      submitButton.classList.add('loading');
+      this.querySelector('.loading-overlay__spinner').classList.remove('hidden');
+
+      const config = fetchConfig('javascript');
+      config.headers['X-Requested-With'] = 'XMLHttpRequest';
+      delete config.headers['Content-Type'];
+
+      const formData = new FormData(this.form);
+      formData.append('sections', this.cartNotification.getSectionsToRender().map((section) => section.id));
+      formData.append('sections_url', window.location.pathname);
+      config.body = formData;
+
+      fetch(`${routes.cart_add_url}`, config)
+        .then((response) => response.json())
+        .then((response) => {
+          if (response.status) {
+            this.handleErrorMessage(response.description);
+            return;
+          }
+
+          this.cartNotification.renderContents(response);
+        })
+        .catch((e) => {
+          console.error(e);
+        })
+        .finally(() => {
+          submitButton.classList.remove('loading');
+          submitButton.removeAttribute('aria-disabled');
+          this.querySelector('.loading-overlay__spinner').classList.add('hidden');
+        });
+    }
+
+    handleErrorMessage(errorMessage = false) {
+      this.errorMessageWrapper = this.errorMessageWrapper || this.querySelector('.product-form__error-message-wrapper');
+      this.errorMessage = this.errorMessage || this.errorMessageWrapper.querySelector('.product-form__error-message');
+
+      this.errorMessageWrapper.toggleAttribute('hidden', !errorMessage);
+
+      if (errorMessage) {
+        this.errorMessage.textContent = errorMessage;
+      }
+    }
+  });
+}
